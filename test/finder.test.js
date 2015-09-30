bbf619a7c8cd291a5f29e85ed6c4e65257e07b87
Nelson <contact.nelsonic@gmail.com>
2015-09-30 23:51:27 +0100

    :boom: initial version done. now lets itterate!

--- test/finder.test.js
+++ test/finder.test.js
@@ -5,12 +5,16 @@ var finder = require('../lib/finder');
 test('Find Profile Link for Anita', function(t) {
   var url = 'www.google.co.uk_search?q=Anita%20Czapla%20Founders';
   var file = require('path').resolve(__dirname + '/../data/' + url + '.html');
-
+  var expected = 'https://uk.linkedin.com/in/anitaczapla';
   fs.readFile(file, function(err, html){
     // console.log(err);
     // console.log(html.toString());
     finder(url, html, function(err, data) {
       console.log(data)
+      var links = data.links.filter(function(link){
+        return link === expected;
+      })
+      t.ok(links[0] === expected, 'Found Profile Link for Anita: '+links[0]);
       t.end();
     });
   })
