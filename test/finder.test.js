8428e1d8795c3adc3708e2907891986cf95a7a71
SimonLab <simon.labondance@gmail.com>
2015-10-05 15:55:50 +0100

    select right links: select pdf link; not select the any-na linkedin links

--- test/finder.test.js
+++ test/finder.test.js
@@ -38,6 +38,26 @@ test('Find Profile Link for Abdi', function(t) {
   })
 })
 
+
+test('Find Profile Link for Abdi', function(t) {
+  var url = 'www.google.co.uk_search?q=Abdi%20Ahmed%20Founders%20and%20Coders';
+  var file = require('path').resolve(__dirname + '/../data/' + url + '.html');
+  var expected = 'https://uk.linkedin.com/pub/abdi-ahmed/100/384/6b0';
+  fs.readFile(file, function(err, html){
+    // console.log(err);
+    // console.log(html.toString());
+    finder(url, html, function(err, data) {
+      console.log(data)
+      var links = data.links.filter(function(link){
+        return link === expected;
+      })
+      t.ok(links[0] === expected, 'Found Profile Link for Abdi: '+links[0]);
+      t.end();
+    });
+  })
+})
+
+
 test('Find Profile Link for Simon', function(t) {
   var url = 'www.google.co.uk_search?q=Simon%20Labondance%20Founders';
   var file = require('path').resolve(__dirname + '/../data/' + url + '.html');
